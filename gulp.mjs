79e3686aaef0fc3d9138f3029c721b37de874b85
sftvov <63662363+sftvov@users.noreply.github.com>
2025-03-21 09:44:10 +0300

    210325

---
+++ gulp.mjs
@@ -0,0 +1,34 @@
+export const font_names = '{Inter,}';
+
+// const maxWidth = 2560;
+// const maxContainer = 1920;
+// const md4 = 1024;
+
+const lightArr = [
+	// '/popups/',
+	// '/site-types/'
+];
+
+const resizeArr = [
+	// ['/products/', false, true, [800, 0], [400, 0], [300, 0], [200, 0]],
+	// ['/site-types/', false, true, [200, 0]],
+];
+
+const retinaArr = [
+	// ['/hero/', 2, [0, 768, 100], [maxWidth, 0, 100]],
+	// ['/site-types/', 2, [maxContainer - 400, 0, 33], [md4, 0, 100]],
+];
+
+export const imageSet = {
+	lightArr: lightArr,
+	resizeArr: resizeArr,
+	retinaArr: retinaArr,
+};
+
+//gulp changeNames
+export const renameObj = {
+	folder: '/img/inspections/items/',
+	name: 'in-',
+};
+// gulp getHtml
+export const pugFile = 'gallery/_gallery_$common';
---
+++ js/script.js
@@ -0,0 +1,107 @@
+const body = document.querySelector('body');
+const wrapper = document.querySelector('.wrapper');
+const page = document.querySelector('.page');
+const header = document.querySelector('.header');
+const specialMenu = document.querySelector('.special-menu');
+const footer = document.querySelector('.footer');
+
+// ----------------------------------------------------------------------
+
+const md1 = getComputedStyle(document.documentElement).getPropertyValue('--md1');
+const md1Query = window.matchMedia(`(min-width: ${md1}px)`);
+const md2 = getComputedStyle(document.documentElement).getPropertyValue('--md2');
+const md2Query = window.matchMedia(`(min-width: ${md2}px)`);
+const md3 = getComputedStyle(document.documentElement).getPropertyValue('--md3');
+const md3Query = window.matchMedia(`(min-width: ${md3}px)`);
+const md4 = getComputedStyle(document.documentElement).getPropertyValue('--md4');
+const md4Query = window.matchMedia(`(min-width: ${md4}px)`);
+const md5 = getComputedStyle(document.documentElement).getPropertyValue('--md5');
+const md5Query = window.matchMedia(`(min-width: ${md5}px)`);
+
+// ----------------------------------------------------------------------
+
+
+//include('../../web-template/src/libs/swiper-bundle-11-2-1.js');
+//include('../../web-template/src/functions/sendmail.js');
+//include('../../web-template/src/functions/isMobile.js');
+//include('../../web-template/src/functions/webp.js');
+//include('../../web-template/src/functions/slide.js');
+//include('../../web-template/src/functions/activator_v3.0.1_now.js');
+//include('../../web-template/src/functions/body_lock.js');
+//include('../../web-template/src/functions/dynamic_adapt.js');
+//include('../../web-template/src/functions/desktopScroll.js');
+//include('../../web-template/src/functions/calculateAge.js');
+//include('../../web-template/src/js-elements/select.js');
+//include('../../web-template/src/js-elements/quantity.js');
+//include('../../web-template/src/js-elements/calendar.js');
+//include('../../web-template/src/js-elements/search.js');
+//include('../../web-template/src/js-elements/indicator.js');
+//include('../../web-template/src/js-elements/tabs_v2.0.0_now.js');
+//include('../../web-template/src/js-elements/popups.js');
+//include('../../web-template/src/js-elements/gallery.js');
+//include('../../web-template/src/js-elements/scroll.js');
+//include('../../web-template/src/js-elements/spollers.js');
+//include('../../web-template/src/js-elements/items_v2.0.0_now.js');
+
+// ----------------------------------------------------------------------
+
+// function headerHeight() {
+// 	let hh = header.offsetHeight + 'px';
+// 	document.documentElement.style.setProperty('--header-height', hh);	
+// }
+
+// const burgerActivator = new Activator('.burger', {
+// 	removed: ['header','.header__container'],
+// 	bodyLock: true,
+// });
+
+// const accessActivator = new Activator('.access', {
+// 	removed: ['.access-menu'],
+// 	effects: 'U',
+// 	effectDuration: 200,
+// 	clickOutClose: true,
+// 	escClose: true,
+// 	// bodyLock: true,
+// });
+
+// const closeActivator = new Activator('._close', {
+// 	removedOwn: ['._removed'],
+// });
+
+// new Activator('._select', {
+// 	//removed: ['#text'],
+// 	removedOwn: ['._select-variants'],
+// 	stops: ['._select-variants'],
+// 	removing: ['._select-variant'],
+// 	clickOutClose: true,
+// 	escClose: true,
+// 	effects: 'U',
+// 	effectDuration: 200,
+// 	//bodyLock: true,
+// 	onlyOne: true,
+// 	//deactivate: false,
+// 	//focus
+// });
+
+// new Activator('._select-variant', {
+//   onlyOne: true,
+//   deactivate: false,
+//   limitContainer: '._select',
+// });
+
+//include('_access.js');
+//include('_swipers.js');
+//include('_map.js');
+
+// window.onload = () => {
+// 	headerHeight();
+// 	updateIndicator('._tabs', '._tabs-title._active', '._tabs-indicator');
+// 	// addTouchClassForMobile();
+// }
+
+// window.onresize = () => {
+// 	headerHeight();
+// 	updateIndicator('._tabs', '._tabs-title._active', '._tabs-indicator');
+// 	// addTouchClassForMobile();
+// }
+
